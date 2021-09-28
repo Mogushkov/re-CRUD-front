5b15a3373de40c28580baaf07138b975826fe3ee
Gapur Mogushkov <gapur@mail.ru>
2021-09-29 02:12:33 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import PostsPage from './components/PostsPage';
+import NewPost from './components/NewPost';
+import ViewPost from './components/ViewPost';
+import IdContext from './context/IdContext';
+import PostEdit from './components/PostEdit';
+
+function App() {
+  const [create] = useState({id: null, content: null, created: null})
+
+  return (
+    <IdContext.Provider value={create}>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route path="/posts/new" component={NewPost} />
+            <Route path="/posts/edit" component={PostEdit} />
+            <Route path="/posts/:id" component={ViewPost} />
+            <Route path="/" component={PostsPage} />
+          </Switch>
+        </div>
+      </Router>
+    </IdContext.Provider>  
+  );
+}
+
+export default App;
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,31 @@
+import {useContext} from 'react';
+import IdContext from '../context/IdContext';
+import {Link} from 'react-router-dom';
+import moment from 'moment';
+import 'moment/locale/ru';
+
+function PostItem({ post }) {
+	const create = useContext(IdContext)
+
+	const onClick = () => {
+		create.id = post.id
+		create.content = post.content
+		create.created = post.created
+	}
+
+    return (
+		<Link to={`/posts/${post.id}`}>
+			<li className="item" onClick={onClick}>
+				<header>
+					<div className="user">
+						<p>Ivan Petrov</p>
+						<p className="date">{moment(new Date(post.created), 'YYYYMMDDhhmm').fromNow()}</p>
+					</div>
+				</header>
+				<div className="content">{post.content}</div>
+			</li>
+	  	</Link>
+    )
+}
+
+export default PostItem
